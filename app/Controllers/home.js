3d4d7a60fe9eb27a7f95f62164d5188f765ffb60
Nathan Baldwin <nathansbaldwin@gmail.com>
2016-02-29 10:38:03 -0600

    opening animation works with hover functions

--- app/Controllers/home.js
+++ app/Controllers/home.js
@@ -1,13 +1,28 @@
 app.controller("home", ["$q", "$http", "$scope", "$firebaseArray", "$state", function($q, $http, $scope, $firebaseArray, $state) {
 
 //home page popopver on hover of homepage graphic:
-  $scope.showPopover=false;
-      
-      $scope.popover = {
-          title: 'Title',
-          message: 'Message'
-      };  
 
+$( document ).ready(function() {
+    console.log( "ready!", Math.max($(document).height(), $(window).height()) );
+
+    console.log("contact height:", $('#contact').height());
+    console.log("technologies height:", $('#technologies').height());
+ 
+  $scope.timer = "";
+  $scope.appState ="";
+      
+  $scope.showNav = () => {
+    if ($scope.timer === "complete") {
+      $scope.appState='nav';
+    }
+  }
+
+  $scope.showSubTitles = () => {
+    if ($scope.timer === "complete") {
+      $scope.appState = 'subtitles';
+    }
+  }
+  
   const backSplash = document.getElementById("i0");
   backSplash.style.strokeDasharray = backSplash.style.strokeDashoffset = backSplash.getTotalLength();
   const signature = document.getElementById("i1");
@@ -18,13 +33,63 @@ app.controller("home", ["$q", "$http", "$scope", "$firebaseArray", "$state", fun
 
   tl.to(backSplash, 3, {strokeDashoffset: 0})
   .to(signature, 2.5, {strokeDashoffset: 0, delay: 0.0, ease: SlowMo.ease.config(0.3, 0.4, false)})
-  .from('.sub-ttl', 1.7, {opacity:0, delay: 0.3, ease: Power1.easeOut});
+  // .from('.sub-ttl', 1.7, {opacity:0, delay: 0.3, ease: Power1.easeOut})
+  .add(()=> {
+    console.log("animation complete");
+    //wait for initial animation to complete before enabling subtitle hover:
+    $scope.timer = 'complete';
+    $scope.appState = 'subtitles';
+    $scope.$apply();
+  });
 
   $scope.scrollToPortfolio = function(time) {
     var scrollTime = time || 1.2;
     console.log("you clicked scrollToPortfolio!");
     var portfolioSec = document.getElementById("portfolio-scroll-anchor");
     var posTop = portfolioSec.offsetTop;
+    console.log("posTop", posTop);
+    // TweenLite.to(window, 2, {scrollTo:{y:400}, ease:Power2.easeOut});
+    TweenLite.to(window, scrollTime, {scrollTo:{y:posTop}, ease:Power3.easeOut});
+  }
+
+  $scope.scrollToAbout = (time) => {
+    var scrollTime = time || 0.9;
+    console.log("you clicked scrollToPortfolio!");
+    var portfolioSec = document.getElementById("about-me-section");
+    var posTop = portfolioSec.offsetTop;
+    console.log("posTop", posTop);
+    // TweenLite.to(window, 2, {scrollTo:{y:400}, ease:Power2.easeOut});
+    TweenLite.to(window, scrollTime, {scrollTo:{y:posTop}, ease:Power3.easeOut});
+  }
+  $scope.scrollToContact = (time) => {
+    var scrollTime = time || 1.5;
+    console.log("you clicked scrollToPortfolio!");
+    var portfolioSec = document.getElementById("contact");
+    var posTop = portfolioSec.offsetTop;
+    console.log("posTop", posTop);
+    // TweenLite.to(window, 2, {scrollTo:{y:400}, ease:Power2.easeOut});
+    TweenLite.to(window, scrollTime, {scrollTo:{y:posTop}, ease:Power3.easeOut});
+  }
+  $scope.scrollToTechnologies = (time) => {
+    console.log("you want to go to technologies");
+    var scrollTime = time || 1.5;
+
+    var docHeight = $(document).height();
+    var contactHeight = $('#contact').innerHeight();
+    var techHeight = $('#technologies').innerHeight();
+
+
+    
+    console.log("you clicked scrollToPortfolio!");
+    var portfolioSec = document.getElementById("technologies");
+
+    // var portfolioHeight = document.getElementById('height').scrollHeight;
+    // console.log("portfolio height:", portfolioHeight);
+
+    var posTop = (docHeight - (contactHeight + techHeight));
+
+    // var posTop = portfolioSec.offsetTop;
+    // console.log("combined height:", portfolioHeight + posTop);
     console.log("posTop", posTop);
     // TweenLite.to(window, 2, {scrollTo:{y:400}, ease:Power2.easeOut});
     TweenLite.to(window, scrollTime, {scrollTo:{y:posTop}, ease:Power3.easeOut});
@@ -47,8 +112,6 @@ app.controller("home", ["$q", "$http", "$scope", "$firebaseArray", "$state", fun
     setTimeout(() => {
       $state.go('home.software');
     }, 55);
-  }
-
-  
-
+  } 
+});
 }]);
