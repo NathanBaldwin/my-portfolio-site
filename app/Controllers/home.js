b25fd60be5cf52ba010f76ad2e729b6c5dbdd95e
Nathan Baldwin <nathansbaldwin@gmail.com>
2016-05-10 22:21:51 -0500

    increased mobile responsiveness

--- app/Controllers/home.js
+++ app/Controllers/home.js
@@ -1,6 +1,5 @@
 app.controller("home", ["$q", "$http", "$scope", "$state", function($q, $http, $scope, $state) {
 
-//home page popopver on hover of homepage graphic:
 
 $( document ).ready(function() {
     console.log( "ready!", Math.max($(document).height(), $(window).height()) );
@@ -33,7 +32,6 @@ $( document ).ready(function() {
 
   tl.to(backSplash, 3, {strokeDashoffset: 0})
   .to(signature, 2.5, {strokeDashoffset: 0, delay: 0.0, ease: SlowMo.ease.config(0.3, 0.4, false)})
-  // .from('.sub-ttl', 1.7, {opacity:0, delay: 0.3, ease: Power1.easeOut})
   .add(function () {
     console.log("animation complete");
     //wait for initial animation to complete before enabling subtitle hover:
@@ -66,32 +64,19 @@ $( document ).ready(function() {
     console.log("you clicked scrollToPortfolio!");
     var portfolioSec = document.getElementById("contact-anchor");
     var posTop = portfolioSec.offsetTop;
-    console.log("posTop", posTop);
-    // TweenLite.to(window, 2, {scrollTo:{y:400}, ease:Power2.easeOut});
     TweenLite.to(window, scrollTime, {scrollTo:{y:posTop}, ease:Power3.easeOut});
   }
   $scope.scrollToTechnologies = function (time) {
-    console.log("you want to go to technologies");
     var scrollTime = time || 1.5;
 
     var docHeight = $(document).height();
     var contactHeight = $('#contact').innerHeight();
     var techHeight = $('#technologies').innerHeight();
-
-
     
-    console.log("you clicked scrollToPortfolio!");
     var portfolioSec = document.getElementById("technologies");
-
-    // var portfolioHeight = document.getElementById('height').scrollHeight;
-    // console.log("portfolio height:", portfolioHeight);
 
     var posTop = (docHeight - (contactHeight + techHeight));
 
-    // var posTop = portfolioSec.offsetTop;
-    // console.log("combined height:", portfolioHeight + posTop);
-    console.log("posTop", posTop);
-    // TweenLite.to(window, 2, {scrollTo:{y:400}, ease:Power2.easeOut});
     TweenLite.to(window, scrollTime, {scrollTo:{y:posTop}, ease:Power3.easeOut});
   }
 
@@ -158,10 +143,7 @@ $( document ).ready(function() {
     triggerHook: "onLeave"
   })
     .setTween("#contact", 0, {className:"-=negative-z"})
-
     .addTo(paralaxController);
   
-
-
   });
 }]);
